b1e134db8fd21427afc77c8d9c7d334b6ea8fcd2
gruelingpine185 <66073333+gruelingpine185@users.noreply.github.com>
2023-12-13 23:32:59 -0500

    added: dynamic array creation

--- inc/faction/utils/darray.h
+++ inc/faction/utils/darray.h
@@ -9,4 +9,14 @@
 
 typedef struct f_darray f_darray;
 
+
+#ifdef __cplusplus
+extern "C" {
+#endif // __cplusplus
+f_res f_create_darray(f_darray** _arr, size_t _soft_cap);
+void f_destroy_darray(f_darray* _arr);
+#ifdef __cplusplus
+}
+#endif // __cplusplus
+
 #endif // FACTION_DARRAY_H
--- src/utils/darray.c
+++ src/utils/darray.c
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+
 #include "utils/darray.h"
 
 
@@ -6,3 +8,33 @@ struct f_darray {
     size_t cap;
     size_t size;
 };
+
+
+f_res f_create_darray(f_darray** _arr, size_t _soft_cap) {
+    if(!_arr) return F_ERR_PARAMS;
+
+    f_darray* arr = (f_darray*) malloc(sizeof(*arr));
+    if(!arr) return F_ERR_MEMORY;
+
+    arr->data = NULL;
+    if(_soft_cap) {
+        arr->data = (void**) malloc(sizeof(*arr->data) * _soft_cap);
+        if(!arr->data) {
+            free(arr);
+            return F_ERR_MEMORY;
+        }
+    }
+
+    arr->cap = _soft_cap;
+    arr->size = 0;
+    *_arr = arr;
+    return F_SUCCESS;
+}
+
+void f_destroy_darray(f_darray* _arr) {
+    if(!_arr) return;
+
+    if(_arr->data) free(_arr->data);
+
+    free(_arr);
+}
