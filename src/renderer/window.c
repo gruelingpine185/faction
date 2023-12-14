13658cea5c3a9209194e5ac76d54116084e33e32
gruelingpine185 <66073333+gruelingpine185@users.noreply.github.com>
2023-12-13 22:33:09 -0500

    added: glfw window creation

--- src/renderer/window.c
+++ src/renderer/window.c
@@ -1,3 +1,5 @@
+#include <stddef.h>
+
 #include <GLFW/glfw3.h>
 
 #include "renderer/window.h"
@@ -9,3 +11,29 @@ struct f_window {
     int w;
     int h;
 };
+
+
+GLFWwindow* f_glfw_create_window(int* _win, int* _h, const char* _title);
+void f_glfw_destroy_window(GLFWwindow* _win);
+
+
+GLFWwindow* f_glfw_create_window(int* _w, int* _h, const char* _title) {
+    if(glfwInit() != GLFW_TRUE) return NULL;
+
+    glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    GLFWwindow* win = glfwCreateWindow(*_w, *_h, _title, NULL, NULL);
+    if(!win) {
+        f_glfw_destroy_window(win);
+        return NULL;
+    }
+
+    glfwGetWindowSize(win, _w, _h);
+    return win;
+}
+
+void f_glfw_destroy_window(GLFWwindow* _w) {
+    glfwDestroyWindow(_w);
+    glfwTerminate();
+}
