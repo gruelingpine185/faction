1e9f3cdcabfb090dfd07d44fbc1d3be398f2bc26
gruelingpine185 <66073333+gruelingpine185@users.noreply.github.com>
2023-12-14 09:01:23 -0500

    added: opaque renderer struct

---
+++ inc/faction/renderer/renderer.h
@@ -0,0 +1,10 @@
+#ifndef FACTION_RENDERER_H
+#define FACTION_RENDERER_H
+
+
+#include "result.h"
+
+
+typedef struct f_renderer f_renderer;
+
+#endif // FACTION_RENDERER_H
---
+++ src/renderer/renderer.c
@@ -0,0 +1,6 @@
+#include <volk/volk.h>
+
+#include "renderer/renderer.h"
+struct f_renderer {
+    VkInstance instance;
+};
