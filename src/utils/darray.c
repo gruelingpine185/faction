ea95a1e9f26f9096efb18bbe03a4a7d387913070
gruelingpine185 <66073333+gruelingpine185@users.noreply.github.com>
2023-12-14 00:32:55 -0500

    added: dynamic array reallocation

--- src/utils/darray.c
+++ src/utils/darray.c
@@ -3,12 +3,36 @@
 #include "utils/darray.h"
 
 
+#ifndef F_DARR_BLOCK_SIZE
+    #define F_DARR_CHUNK_SIZE 16
+#endif // F_DARR_BLOCK_SIZE
+
+
 struct f_darray {
     void** data;
     size_t cap;
     size_t size;
 };
 
+
+f_res f_realloc_darray(f_darray* _arr);
+
+
+f_res f_realloc_darray(f_darray* _arr) {
+    if(!_arr) return F_ERR_PARAMS;
+
+    size_t new_cap = (!_arr->cap)?
+        F_DARR_CHUNK_SIZE: _arr->cap * 2;
+    void** new_data = (!_arr->data)?
+        (void**) malloc(sizeof(*_arr->data) * new_cap):
+        (void**) realloc(_arr->data, new_cap);
+
+    if(!new_data) return F_ERR_MEMORY;
+
+    _arr->cap = new_cap;
+    _arr->data = new_data;
+    return F_SUCCESS;
+}
 
 f_res f_create_darray(f_darray** _arr, size_t _soft_cap) {
     if(!_arr) return F_ERR_PARAMS;
