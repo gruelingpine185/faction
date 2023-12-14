35a23a227ac8d5ae7bc2aae54a041d0fa9db11e8
gruelingpine185 <66073333+gruelingpine185@users.noreply.github.com>
2023-12-14 09:04:58 -0500

    added: create VkInstance

--- src/renderer/renderer.c
+++ src/renderer/renderer.c
@@ -1,5 +1,6 @@
 #include <stddef.h>
 #include <stdint.h>
+
 #include <volk/volk.h>
 #include <GLFW/glfw3.h>
 
@@ -10,7 +11,48 @@
 struct f_renderer {
     VkInstance instance;
 };
+
+
+VkInstance f_vk_create_instance(const char* _title);
+void f_vk_destroy_instance(VkInstance _instance);
 f_darray* f_get_vk_req_instance_exts(void);
+
+
+VkInstance f_vk_create_instance(const char* _title) {
+    VkApplicationInfo app_info = {0};
+    app_info.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    app_info.pApplicationName = _title;
+    app_info.applicationVersion = VK_MAKE_API_VERSION(0, 1, 0, 0);
+    app_info.pEngineName = "Faction Engine";
+    app_info.engineVersion = VK_MAKE_API_VERSION(0, 1, 0, 0);
+    app_info.apiVersion = VK_API_VERSION_1_3;
+
+    f_darray* arr = f_get_vk_req_instance_exts();
+    if(!arr) return NULL;
+
+    VkInstanceCreateInfo create_info = {0};
+    create_info.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+#ifdef __APPLE__
+    create_info.flags |= VK_INSTANCE_CREATE_ENUMERATE_PORTABILITY_BIT_KHR;
+#endif // __APPLE__
+    create_info.pApplicationInfo = &app_info;
+    create_info.enabledExtensionCount =
+        (uint32_t) f_get_darray_size(arr, NULL);
+    create_info.ppEnabledExtensionNames =
+        (const char**) f_get_darray_data(arr, NULL);
+    VkInstance instance = NULL;
+    if(vkCreateInstance(&create_info, NULL, &instance) != VK_SUCCESS) {
+        return NULL;
+    }
+
+    f_destroy_darray(arr);
+    return instance;
+}
+
+void f_vk_destroy_instance(VkInstance _instance) {
+    vkDestroyInstance(_instance, NULL); 
+}
+
 f_darray* f_get_vk_req_instance_exts(void) {
     uint32_t glfw_count = 0;
     const char** glfw_exts = glfwGetRequiredInstanceExtensions(&glfw_count);
