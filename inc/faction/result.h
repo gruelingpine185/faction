9eb93c777f45e9e4497b199363eb7e5879d449f5
gruelingpine185 <66073333+gruelingpine185@users.noreply.github.com>
2023-12-13 20:53:21 -0500

    added status codes

---
+++ inc/faction/result.h
@@ -0,0 +1,12 @@
+#ifndef FACTION_RESULT_H
+#define FACTION_RESULT_H
+
+
+typedef enum {
+    F_SUCCESS,
+    F_ERR_PARAMS,
+    F_ERR_BOUNDS,
+    F_ERR_MEMORY
+} f_res;
+
+#endif // FACTION_RESULT_H
