8526131ad2a1ff0599cc877c74a4016de9ec50a0
gruelingpine185 <66073333+gruelingpine185@users.noreply.github.com>
2023-12-15 09:49:38 -0500

    create renderer

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <stddef.h>
 
 #include "renderer/window.h"
+#include "renderer/renderer.h"
 
 
 int main(void) {
@@ -8,9 +9,15 @@ int main(void) {
     f_res res = f_create_window(&win, 600, 600, "hello world");
     if(res != F_SUCCESS) return 1;
 
+    f_renderer* renderer = NULL;
+    res = f_create_renderer(&renderer, win);
+    if(res != F_SUCCESS) return 1;
+
     while(f_is_window_open(win)) {
         f_window_poll_events();
     }
 
+    f_destroy_renderer(renderer);
+    f_destroy_window(win);
     return 0;
 }
