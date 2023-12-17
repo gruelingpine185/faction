586e00f8baff2bd537ed1574271c8f4678f0ff92
gruelingpine185 <66073333+gruelingpine185@users.noreply.github.com>
2023-12-17 00:17:27 -0500

    extracted vulkan structure setup

--- src/renderer/renderer.c
+++ src/renderer/renderer.c
@@ -19,13 +19,19 @@ struct f_renderer {
 };
 
 
+VkApplicationInfo f_vk_create_app_info(const char* _title);
+VkInstanceCreateInfo f_vk_create_create_info(const VkApplicationInfo* _app_info,
+                                                f_darray* _exts,
+                                                f_darray* _layers,
+                                                f_res* _res);
 VkInstance f_vk_create_instance(const char* _title, f_res* _res);
 void f_vk_destroy_instance(VkInstance _instance);
 f_darray* f_get_vk_req_instance_exts(f_res* _res);
 f_darray* f_get_vk_v_layers(f_res* _res);
 
 
-VkInstance f_vk_create_instance(const char* _title, f_res* _res) {
+
+VkApplicationInfo f_vk_create_app_info(const char* _title) {
     VkApplicationInfo app_info = {0};
     app_info.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
     app_info.pApplicationName = _title;
@@ -33,41 +39,64 @@ VkInstance f_vk_create_instance(const char* _title, f_res* _res) {
     app_info.pEngineName = "Faction Engine";
     app_info.engineVersion = VK_MAKE_API_VERSION(0, 1, 0, 0);
     app_info.apiVersion = VK_API_VERSION_1_3;
+    return app_info;
+}
 
-    f_res res;
-    f_darray* arr = f_get_vk_req_instance_exts(&res);
-    F_CHECK(arr, &res, res, NULL)
-
-    const char* v_layers[] = {"VK_LAYER_KHRONOS_validation"};
+VkInstanceCreateInfo f_vk_create_create_info(const VkApplicationInfo* _app_info,
+                                                f_darray* _exts,
+                                                f_darray* _layers,
+                                                f_res* _res) {
     VkInstanceCreateInfo create_info = {0};
+    F_CHECK(_exts, _res, F_ERR_PARAMS, create_info);
+
     create_info.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
 #ifdef __APPLE__
     create_info.flags |= VK_INSTANCE_CREATE_ENUMERATE_PORTABILITY_BIT_KHR;
 #endif // __APPLE__
-    create_info.pApplicationInfo = &app_info;
+    create_info.pApplicationInfo = _app_info;
     create_info.enabledExtensionCount =
-        (uint32_t) f_get_darray_size(arr, NULL);
+        (uint32_t) f_get_darray_size(_exts, NULL);
     create_info.ppEnabledExtensionNames =
-        (const char**) f_get_darray_data(arr, NULL);
-    create_info.enabledLayerCount = (sizeof(v_layers) / sizeof(*v_layers));
-    create_info.ppEnabledLayerNames = v_layers; 
+        (const char**) f_get_darray_data(_exts, NULL);
+#if F_DEBUG_MODE
+    create_info.enabledLayerCount =
+        (uint32_t) f_get_darray_size(_layers, NULL);
+    create_info.ppEnabledLayerNames =
+        (const char**) f_get_darray_data(_layers, NULL);
+#endif // F_DEBUG_MODE
+    if(_res) *_res = F_SUCCESS;
 
-#if !NDEBUG
+    return create_info;
+}
+
+VkInstance f_vk_create_instance(const char* _title, f_res* _res) {
+    f_res res;
+    f_darray* exts = f_get_vk_req_instance_exts(&res);
+    F_CHECK(exts, &res, res, NULL)
+
+    f_darray* v_layers = f_get_vk_v_layers(&res);
+    VkApplicationInfo app_info = f_vk_create_app_info(_title);
+    VkInstanceCreateInfo create_info = f_vk_create_create_info(&app_info,
+                                                                exts,
+                                                                v_layers,
+                                                                &res);
+#if F_DEBUG_MODE
     printf("Required Extensions:\n");
-    for(size_t i = 0; i < create_info.enabledExtensionCount; i++) {
-        printf("    %s\n", (const char*) f_get_darray_at(arr, i, NULL));
+    for(size_t i = 0; i < f_get_darray_size(exts, NULL); i++) {
+        printf("    %s\n", (const char*) f_get_darray_at(exts, i, NULL));
     }
 
     printf("Enabled Layers:\n");
-    for(size_t i = 0; i < create_info.enabledLayerCount; i++) {
-        printf("    %s\n", create_info.ppEnabledLayerNames[i]);
+    for(size_t i = 0; i < f_get_darray_size(v_layers, NULL); i++) {
+        printf("    %s\n", (const char*) f_get_darray_at(v_layers, i, NULL));
     }
-#endif // NDEBUG
+#endif // F_DEBUG_MODE
     VkInstance instance = NULL;
     VkResult vk_res = vkCreateInstance(&create_info, NULL, &instance);
     F_CHECK(vk_res == VK_SUCCESS, _res, F_ERR_INTERNAL, NULL);
 
-    f_destroy_darray(arr);
+    f_destroy_darray(exts);
+    f_destroy_darray(v_layers);
     return instance;
 }
 
