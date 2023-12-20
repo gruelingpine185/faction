0133815a308e0a89e2300699c43d51ee4ca14652
gruelingpine185 <66073333+gruelingpine185@users.noreply.github.com>
2023-12-20 01:06:54 -0500

    added: top-level error message printing

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <stddef.h>
+#include <stdio.h>
 
 #include "renderer/window.h"
 #include "renderer/renderer.h"
@@ -7,11 +8,17 @@
 int main(void) {
     f_window* win = NULL;
     f_res res = f_create_window(&win, 600, 600, "hello world");
-    if(res != F_SUCCESS) return 1;
+    if(res != F_SUCCESS) {
+       printf("Faction: failed to create window. [%d]\n", res);
+        return 1;
+    }
 
     f_renderer* renderer = NULL;
     res = f_create_renderer(&renderer, win);
-    if(res != F_SUCCESS) return 1;
+    if(res != F_SUCCESS) {
+       printf("Faction: failed to create renderer. [%d]\n", res);
+        return 1;
+    }
 
     while(f_is_window_open(win)) {
         f_window_poll_events();
