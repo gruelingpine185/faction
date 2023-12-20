bb43dd5b76dec5b8b376fc8f0c51378ca89c9490
gruelingpine185 <66073333+gruelingpine185@users.noreply.github.com>
2023-12-20 12:27:43 -0500

    fixed: items left undestroyed on error

--- src/renderer/renderer.c
+++ src/renderer/renderer.c
@@ -89,6 +89,7 @@ VkInstance f_vk_create_instance(const char* _title, f_res* _res) {
     if(!f_vk_check_supported_v_layers(v_layers, &res)) {
         if(_res) *_res = res;
 
+        f_destroy_darray(exts);
         f_destroy_darray(v_layers);
         return NULL;
     }
@@ -108,6 +109,8 @@ VkInstance f_vk_create_instance(const char* _title, f_res* _res) {
     if(res != F_SUCCESS) {
         if(_res) *_res = res;
 
+        f_destroy_darray(v_layers);
+        f_destroy_darray(exts);
         return NULL;
     }
 #if F_DEBUG_MODE
