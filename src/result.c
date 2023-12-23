51bc8874d894d37df2511cec24ecc354e1f6bbc1
gruelingpine185 <66073333+gruelingpine185@users.noreply.github.com>
2023-12-23 14:51:58 -0500

    added: error code for no Vulkan supported GPUs

--- src/result.c
+++ src/result.c
@@ -3,11 +3,18 @@
 
 const char* f_res_to_str(f_res _res) {
     switch (_res) {
-        case F_SUCCESS:         return "Success";
-        case F_ERR_PARAMS:      return "Invalid parameter(s)";
-        case F_ERR_BOUNDS:      return "Value out of bounds";
-        case F_ERR_MEMORY:      return "Insufficient memory";
-        case F_ERR_INTERNAL:    return "Unsuccessful internal operation";
+        case F_SUCCESS:
+            return "Success";
+        case F_ERR_PARAMS:
+            return "Invalid parameter(s)";
+        case F_ERR_BOUNDS:
+            return "Value out of bounds";
+        case F_ERR_MEMORY:
+            return "Insufficient memory";
+        case F_ERR_INTERNAL:
+            return "Unsuccessful internal operation";
+        case F_ERR_NO_SUPPORTED_GPU:
+            return "GPUs with Vulkan support not found";
         default: break;
     }
 
