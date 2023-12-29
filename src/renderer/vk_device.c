a1e74f4ea90ae7ca41ed1a4084bbd72954bb8f52
gruelingpine185 <66073333+gruelingpine185@users.noreply.github.com>
2023-12-29 18:57:46 -0500

    added: printing of physical devices

--- src/renderer/vk_device.c
+++ src/renderer/vk_device.c
@@ -1,5 +1,8 @@
 #include <stddef.h>
 #include <stdlib.h>
+#if F_DEBUG_MODE
+#include <stdio.h>
+#endif // F_DEBUG_MODE
 
 #include "renderer/vk_device.h"
 
@@ -17,7 +20,14 @@ VkPhysicalDevice* f_get_vk_p_devices(VkInstance _instance,
     F_CHECK(devices, _res, F_ERR_MEMORY, NULL);
 
     vkEnumeratePhysicalDevices(_instance, &count, devices);
+#if F_DEBUG_MODE
+    printf("Physical Devices (GPUs):\n");
+    for(uint32_t i = 0; i < count; i++) {
+        VkPhysicalDeviceProperties prop = {0};
+        vkGetPhysicalDeviceProperties(devices[i], &prop);
+        printf("    %s\n", prop.deviceName);
     }
+#endif // F_DEBUG_MODE
     if(_res) *_res = F_SUCCESS;
     if(_count) *_count = count;
     
